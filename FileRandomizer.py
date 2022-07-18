6b8f37ff019775ea62c4775c19f2f4ddb843700e
JBDCE <61785226+JBDCE@users.noreply.github.com>
2022-07-18 19:59:30 +0200

    Attempt on file validation

--- FileRandomizer.py
+++ FileRandomizer.py
@@ -1,8 +1,6 @@
+import hashlib
 import os
 import random
-
-
-# TODO Add Content
 
 # Create a list of all the files in the folder
 fileList = []
@@ -12,27 +10,41 @@ for file in os.listdir("/"):
         print("Found file: ", file)
         fileList.append(file)
 
-
 # Assert all the files have the same file extension
 checkExt = os.path.splitext(fileList[0])
 
 for file in fileList:
     _, ext = os.path.splitext(file)
-    assert ext==checkExt, "Error Asserting filenames. Are the files the same type?"
+    assert ext==checkExt, \
+        "Error Asserting filenames. Are the files the same type?"
 
 # Generate md5 for all the files
+md5List = []
+for file in fileList:
+    # Loading the whole file like this is not good
+    md5List.append(hashlib.md5(open(file).read())) 
 
 # Shuffle the filenames
-ranFileList = random.sample(fileList)
-# Just for good measure
-random.shuffle(ranFileList)
-random.shuffle(ranFileList)
-random.shuffle(ranFileList)
-random.shuffle(ranFileList)
+randFileList = random.sample(fileList)
+
+# This is bad.
+# Instead just run the file multiple times
+# # Just for good measure
+# random.shuffle(ranFileList)
+# random.shuffle(ranFileList)
+# random.shuffle(ranFileList)
+# random.shuffle(ranFileList)
 
 # Rename the files
-for orig, rand in zip(fileList, ranFileList):
+for orig, rand in zip(fileList, randFileList):
     os.rename(orig, rand)
 
-
 # Verify the files still have the same md5
+randMd5List = []
+for file in randFileList:
+    # Same memory issue as previously
+    randMd5List.append(hashlib.md5(open(file).read()))
+
+for a, b in zip(md5List, randMd5List):
+    if a!=b: print("Error", end="")
+    print("A: {0} B: {1}", a, b)
